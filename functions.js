96552b76f53f9554d4c0ace70bd37dfd93ab6b15
greenpro <craigcode1010@gmail.com>
2026-05-17 20:20:00 -0500

    style

---
+++ functions.js
@@ -0,0 +1,13 @@
+"use strict";
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var icon = document.getElementById("icon");
+    if (icon && menu && menu.style.display === "block") {
+        menu.style.display = "none";
+        icon.style.paddingBottom = "40px";
+    }
+    else if (icon && menu) {
+        menu.style.display = "block";
+        icon.style.paddingBottom = "0px";
+    }
+}
