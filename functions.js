648f56af7309fdd85d74f5d33e359bbdb97436e9
greenpro <craigcode1010@gmail.com>
2026-05-18 16:14:38 -0500

    style

--- functions.js
+++ functions.js
@@ -1,13 +1,10 @@
 "use strict";
 function toggleMenu() {
     var menu = document.getElementById("menu");
-    var icon = document.getElementById("icon");
-    if (icon && menu && menu.style.display === "block") {
+    if (menu && menu.style.display === "block") {
         menu.style.display = "none";
-        icon.style.paddingBottom = "40px";
     }
-    else if (icon && menu) {
+    else if (menu) {
         menu.style.display = "block";
-        icon.style.paddingBottom = "0px";
     }
 }
